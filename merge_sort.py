106959fc2b8ad44a4ae109f40d453fbda724286b
MaxIakovliev <max.iakovliev@gmail.com>
2015-11-09 23:07:41 +0800

    update

--- merge_sort.py
+++ merge_sort.py
@@ -2,7 +2,7 @@ def mergeSort(arr):
     
     if len(arr)>1:
         mid = len(arr)//2
-        lefthalf = arr[:mid]
+        left = arr[:mid]
         right = arr[mid:]
 
         mergeSort(left)
@@ -31,6 +31,49 @@ def mergeSort(arr):
             k=k+1
 
 
-arr = [55,22,65,24,76,223,7,2,75,213,8]
-mergeSort(arr)
+def mergeSortDesc(arr):
+    
+    if len(arr)>1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i= len(left)-1
+        j=len(right)-1
+        k=len(left)-1
+        while i >=0 and j >=0:
+            if left[i] < right[j]:
+                arr[k]=left[i]
+                i=i-1
+            else:
+                arr[k]=right[j]
+                j=j-1
+            k=k-1
+		
+        print(left)
+		
+        i= len(left)-1
+        j=len(right)-1
+        k=len(left)-1
+			
+        while i >=0:            
+            arr[k]=left[i]
+            i=i-1
+            k=k-1
+
+        i= len(left)-1
+        j=len(right)-1
+        k=len(left)-1
+
+        while j >=0:
+            arr[k]=right[j]
+            j=j-1
+            k=k-1		
+			
+
+arr = [55,22,65,24,76,223,7,2,75,213]
+mergeSortDesc(arr)
 print(arr)
