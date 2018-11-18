842a26cfc55164772d13d37b1d1d9f107ffc5dde
Max <max.iakovliev@gmail.com>
2018-11-18 20:41:34 +0800

    accepted solution

---
+++ Session002/DepthFirstSearch/SecondMinimumNodeInaBinaryTree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findSecondMinimumValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def bfs(node):
+            if node is None:
+                return
+            
+            if root.val< node.val and node.val<self.secondMin:
+                self.secondMin=node.val
+            bfs(node.left)
+            bfs(node.right)
+            
+        self.secondMin=float('inf')
+        bfs(root)
+        if self.secondMin is None:
+            return -1
+        return self.secondMin
